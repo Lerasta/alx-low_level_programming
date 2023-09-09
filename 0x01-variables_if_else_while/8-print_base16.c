27e0b9c3db97b2ef5909889ee7dcc0fbf4531936
khalid <elmourtaqik@gmail.com>
2023-09-08 17:53:33 -0700

    bases

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Description: Print all the letters except q and e
+ *Return: always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+		for (n = 48; n <= 57; n++)
+		{
+			putchar(n);
+		}
+		for (m = 97; m <= 182; m++)
+		{
+			putchar (m);
+		}
+		putchar('\n');
+		return (0);
+}
