cdc7f6f01d9713764dbe0b7971fdf05c171701c9
khalid <elmourtaqik@gmail.com>
2023-09-08 18:01:19 -0700

    bas

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 	int n;
 	int m;
 
-		for (n = 48; n <= 57; n++)
+		for (n = 48; n <= 58; n++)
 		{
 			putchar(n);
 		}
-		for (m = 97; m <= 182; m++)
+		for (m = 97; m <= 103; m++)
 		{
 			putchar (m);
 		}
